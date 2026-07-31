83bcbf503ab13c8e652a321154237b8c377f5dd9
mohammedabdullaga <mohammed.abdulla.ga@gmail.com>
2026-07-31 15:05:47 +0300

    Refine CORS and private network support for frontend

--- server.js
+++ server.js
@@ -19,21 +19,23 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error('CORS policy does not allow access from the specified origin.'), false);
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  }),
-);
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error('CORS policy does not allow access from the specified origin.'), false);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
 
-app.options('*', cors({ origin: allowedOrigins, optionsSuccessStatus: 204 }));
+app.use(cors(corsOptions));
+app.options('*', cors({
+  ...corsOptions,
+  optionsSuccessStatus: 204,
+}));
 app.use(express.json());
 app.use('/api', apiRoutes);
 
